bca8c0a9540aea6119ef82221b6dc2f6c9171105
KRIS-PC2 <hropenmail01@abv.bg>
2021-08-31 02:24:46 +0300

    DiscordBot project

---
+++ Apocryph.Dao.Bot.DiscordBotService/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text
+
+
+namespace Apocryph.Dao.Bot.DiscordBotService
+{
+	public class Program
+	{
+
+	}
+}
---
+++ discord_bot/Class1.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Apocryph.Dao.Bot.DiscordBotService
+{
+
+}
