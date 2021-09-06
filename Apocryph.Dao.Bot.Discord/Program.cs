3f0152f6c26601330e217c53a300e33b5e08a926
KRIS-PC2 <hropenmail01@abv.bg>
2021-09-06 20:30:53 +0300

    Logging, Discord bot boot outline, Discord.NET added

--- Apocryph.Dao.Bot.Discord/Program.cs
+++ Apocryph.Dao.Bot.Discord/Program.cs
@@ -1,13 +1,50 @@
 ﻿using System;
-using Discord.Net;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace Apocryph.Dao.Bot.Discord
 {
 	class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			// We like async ops a lot
+			new Program().MainAsync().GetAwaiter().GetResult();
+		}
 
+		public async Task MainAsync()
+		{
+			// 
+			// NOTE (from https://docs.stillu.cc/guides/getting_started/first-bot.html):
+			// If your application throws any exceptions within an async context, they will be thrown all the way back up to the first non-async method;
+			// since our first non-async method is the program's Main method, this means that all unhandled exceptions will be thrown up there,
+			// which will crash your application.
+			// Discord.Net will prevent exceptions in event handlers from crashing your program, but any exceptions in your async main will cause the application to crash.
+			// 
+
+			// Async context
+
+			var config = new DiscordSocketConfig();
+			//TODO: setup config
+
+			using (var client = new DiscordSocketClient(config))
+			{
+				client.Log += Log;
+
+				//TODO: subscribe stuff here...
+				//TODO: listening loop?
+			}
+		}
+
+		/// <summary>
+		/// Catch log events and print to console. Subscribe to the discord bot client.
+		/// </summary>
+		private Task Log(LogMessage msg)
+		{
+			// Discord.NET uses proprietary LogMessage for error handling
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
 		}
 	}
 }
