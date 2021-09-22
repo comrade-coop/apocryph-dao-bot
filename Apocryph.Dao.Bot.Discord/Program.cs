9505f50660266518dbf00b77dc34e8720224a51b
KRIS-PC2 <hropenmail01@abv.bg>
2021-09-23 01:02:34 +0300

    Discord bot is ready to take in commands. Gitignore changes.

--- Apocryph.Dao.Bot.Discord/Program.cs
+++ Apocryph.Dao.Bot.Discord/Program.cs
@@ -2,11 +2,14 @@
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using Nethereum.JsonRpc.Client;
 
 namespace Apocryph.Dao.Bot.Discord
 {
 	class Program
 	{
+		private static DiscordSocketClient CLIENT;
+
 		public static void Main(string[] args)
 		{
 			// We like async ops a lot
@@ -34,6 +37,7 @@ namespace Apocryph.Dao.Bot.Discord
 			{
 				using (var client = new DiscordSocketClient(config))
 				{
+					CLIENT = client;
 					client.Log += LogDiscordMessage;
 
 					//  You can assign your bot token to a string, and pass that in to connect.
@@ -44,6 +48,7 @@ namespace Apocryph.Dao.Bot.Discord
 					await client.StartAsync();
 
 					//TODO: implement network commands, add in netherium hooks
+					client.MessageReceived += MessageReceivedAsync;
 
 					// Block this task until the program is closed.
 					await Task.Delay(-1);
@@ -54,6 +59,21 @@ namespace Apocryph.Dao.Bot.Discord
 			{
 				//TODO: More descriptive local files requirements
 				Log($"Initialize local files first = {(int) localFiles.GetRequiredInitalizations()}");
+			}
+		}
+
+		private async Task MessageReceivedAsync(SocketMessage message)
+		{
+			//This ensures we don't loop things by responding to ourselves (as the bot)
+			if (message.Author.Id == CLIENT?.CurrentUser.Id)
+				return;
+
+			switch(message.Content)
+			{
+				case ".HelloApocryph":
+					await message.Channel.SendMessageAsync($"Hello {message.Author.Username}");
+					break;
+				//TODO: Add token trading commands here
 			}
 		}
 
