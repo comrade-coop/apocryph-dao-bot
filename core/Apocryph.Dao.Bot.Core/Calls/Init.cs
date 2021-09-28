5b972e853acb3bc2d5c44aaa6283706ee7a06e57
Branimir Angelov <55453003+branimirangelov@users.noreply.github.com>
2021-09-29 00:02:04 +0300

    feat: ingest events from rinkeby

--- core/Apocryph.Dao.Bot.Core/Calls/Init.cs
+++ core/Apocryph.Dao.Bot.Core/Calls/Init.cs
@@ -14,10 +14,10 @@ namespace Apocryph.Dao.Bot.Core.Calls
 
         public async Task RunAsync()
         {
-            var transactions = await context.StreamFunctionAsync<DaoTransaction>(nameof(EthereumTransactions), Array.Empty<object>());
-            await foreach (var transaction in transactions)
+            var transferEvents = await context.StreamFunctionAsync<TransferEventDTO>(nameof(TransferEvents), Array.Empty<object>());
+            await foreach (var transferEvent in transferEvents)
             {
-                Console.WriteLine($"Ingested: {transaction.BlockHash}");
+                Console.WriteLine($"Ingested: event Transfer(from: {transferEvent.From}, to: {transferEvent.To}, value: {transferEvent.Value})");
             }
         }
     }
