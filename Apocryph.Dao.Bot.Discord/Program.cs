4a5c9064679f773d4353d8d3d8142deb98563f90
KRIS-PC2 <hropenmail01@abv.bg>
2021-10-12 00:52:13 +0300

    Cleaning up

--- Apocryph.Dao.Bot.Discord/Program.cs
+++ Apocryph.Dao.Bot.Discord/Program.cs
@@ -3,7 +3,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
-using Nethereum.JsonRpc.Client;
 
 namespace Apocryph.Dao.Bot.Discord
 {
--- discord_bot/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Apocryph.Dao.Bot.DiscordBotService
-{
-
-}
