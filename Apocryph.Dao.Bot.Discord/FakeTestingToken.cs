0e5e5216a4ba38e5cecbc90c9e87955d0822999a
Kris Development <35272196+KrisDv@users.noreply.github.com>
2021-10-14 20:09:03 +0300

    feat: initial discord bot integration
    resolves #1 for 5 points

---
+++ Apocryph.Dao.Bot.Discord/FakeTestingToken.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Apocryph.Dao.Bot.Discord
+{
+	/// <summary>
+	/// Note: This class is just for testing bot commands
+	/// </summary>
+	class FakeTestingToken
+	{
+		public Dictionary<string, int> data = new Dictionary<string, int>();
+		public Dictionary<string, string> IdToUser = new Dictionary<string, string>();
+
+		public bool Pay(string fromUser, string toUserId, int amount, out string msg)
+		{
+			try
+			{
+				data[fromUser] -= amount;
+				data[IdToUser[toUserId]] += amount;
+				msg = $"Transfered {amount} from {fromUser} to {toUserId}";
+				return true;
+			}
+			catch (Exception e)
+			{
+				msg = e.ToString();
+			}
+
+			return false;
+		}
+
+		public float Bal(string user, out bool error, out string msg)
+		{
+			try
+			{
+				var value = data[user];
+				error = false;
+				msg = string.Empty;
+				return value;
+			}
+			catch (Exception e)
+			{
+				msg = e.ToString();
+			}
+
+			error = true;
+			return 0;
+		}
+
+		internal bool Add(string username, int bal, out string userId)
+		{
+			if (data.ContainsKey(username))
+			{
+				userId = string.Empty;
+				return false;
+			}
+
+			userId = data.Count.ToString();
+			data.Add(username, bal);
+			IdToUser.Add(userId, username);
+			return true;
+		}
+	}
+}
---
+++ Apocryph.Dao.Bot.Discord/LocalFiles.cs
@@ -0,0 +1,119 @@
+﻿using System.IO;
+
+namespace Apocryph.Dao.Bot.Discord
+{
+	/// <summary>
+	/// Handle sensitive data
+	/// </summary>
+	internal sealed class LocalFiles
+	{
+		public enum RequiredInitalizations
+		{
+			None = 0,
+			DiscordToken = 1,
+		}
+
+		private const string _discordAuthTokenFileName = "token.auth";
+		public string DiscordBotToken;
+
+		public bool LocalFilesReady => GetRequiredInitalizations() == RequiredInitalizations.None;
+
+
+		//-------DEBUG----------
+		private const string _fakeTokenStateFileName = "state.fake";
+		public FakeTestingToken FakeTokenState = new FakeTestingToken();
+		//----------------------
+
+
+		public LocalFiles()
+		{
+			CreateFilesIfNotExist();
+			LoadFileContents();
+		}
+
+		private void CreateFilesIfNotExist ()
+		{
+			if (!Exists(_discordAuthTokenFileName))
+			{
+				WriteTextToFile(_discordAuthTokenFileName, string.Empty);
+			}
+
+			//-------DEBUG----------
+			if (!Exists(_fakeTokenStateFileName))
+			{
+				WriteTextToFile(_fakeTokenStateFileName, string.Empty);
+			}
+			//----------------------
+		}
+
+		private void LoadFileContents()
+		{
+			ReadTextFromFile(_discordAuthTokenFileName, out DiscordBotToken);
+
+			//-------DEBUG----------
+			string tokenStateRaw;
+			ReadTextFromFile(_fakeTokenStateFileName, out tokenStateRaw);
+			FakeTokenState = ((FakeTestingToken) Newtonsoft.Json.JsonConvert.DeserializeObject(tokenStateRaw)) ?? new FakeTestingToken();
+			//----------------------
+		}
+
+		public void SaveFileContents ()
+		{
+			//-------DEBUG----------
+			WriteTextToFile(_fakeTokenStateFileName, Newtonsoft.Json.JsonConvert.SerializeObject(FakeTokenState));
+			//----------------------
+		}
+
+		public RequiredInitalizations GetRequiredInitalizations()
+		{
+			var output = RequiredInitalizations.None;
+
+			if (string.IsNullOrEmpty(DiscordBotToken))
+			{
+				output |= RequiredInitalizations.DiscordToken;
+			}
+
+			return output;
+		}
+
+		private static string ParseToAbsolutePath(string path)
+		{
+			return Path.GetFullPath(path);
+		}
+
+		private static void WriteTextToFile(string path, string content)
+		{
+			string _filePath = ParseToAbsolutePath(path);
+			FileInfo _fileInfo = new FileInfo(_filePath);
+
+			_fileInfo.Directory.Create();
+			StreamWriter _writer = File.CreateText(_filePath);
+			_writer.Close();
+			File.WriteAllText(_filePath, content);
+		}
+
+		/// <summary>
+		/// Outputs a string result and returns true if it was successful
+		/// </summary>
+		private static bool ReadTextFromFile(string path, out string content)
+		{
+			string _filePath = ParseToAbsolutePath(path);
+			content = string.Empty;
+
+			if (File.Exists(_filePath))
+			{
+				content = File.ReadAllText(_filePath);
+				return true; //read success
+			}
+
+			return false; //read failed
+		}
+
+		private static bool Exists(string path)
+		{
+			string _filePath = ParseToAbsolutePath(path);
+			return File.Exists(_filePath);
+		}
+
+	}
+}
