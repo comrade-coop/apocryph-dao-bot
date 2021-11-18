c213955f8988698c4109980c49abc468c2f007ac
Kamil Wojciechowski <aphex5@protonmail.com>
2021-11-18 11:15:57 +0100

    #added stream and validation tests

--- Apocryph.Dao.Bot.Tests/Streams/GetBalanceDialogStreamTests.cs
+++ Apocryph.Dao.Bot.Tests/Streams/GetBalanceDialogStreamTests.cs
@@ -4,14 +4,10 @@ using Apocryph.Dao.Bot.Tests.Fixtures;
 using FluentAssertions;
 using NUnit.Framework;
 
-namespace Apocryph.Dao.Bot.Tests.Scenarios
+namespace Apocryph.Dao.Bot.Tests.Streams
 {
-    public class GetBalanceDialogStreamValidation : PerperFixture
+    public class GetBalanceDialogStreamTests : PerperFixture
     {
-        private static string _session;
-        private string _address = "0x699608158E4B13f98ad99EAb5Ccd65d2bfc2a333";
-        private string _signature = "0x6bd5a6bbd46c255dbd9ec22bf56c3dc24bf9a5aafe136d660329a67c0c87462b6f944c49b7bb0f4ac0edd8a19d87842b5141986d8c31584bf0c06a92c686d6541b";
-        private string _userName = "TestUser";
         private ulong _userId = 1000L;
         
         [Test]
@@ -27,8 +23,3 @@ namespace Apocryph.Dao.Bot.Tests.Scenarios
         }
     }
 }
-
-// TODO:
-// GetBalanceDialogStream
-// IntroAttemptDialogStream
-// IntroChallengeMessage
---
+++ Apocryph.Dao.Bot.Tests/Streams/IntroAttemptDialogStreamTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Apocryph.Dao.Bot.Message;
+using Apocryph.Dao.Bot.Tests.Fixtures;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Apocryph.Dao.Bot.Tests.Streams
+{
+    public class IntroAttemptDialogStreamTests : PerperFixture
+    {
+        private static string _session = "__session__";
+        private string _address = "0x699608158E4B13f98ad99EAb5Ccd65d2bfc2a333";
+        private string _signature = "0x6bd5a6bbd46c255dbd9ec22bf56c3dc24bf9a5aafe136d660329a67c0c87462b6f944c49b7bb0f4ac0edd8a19d87842b5141986d8c31584bf0c06a92c686d6541b";
+        
+        [Test]
+        public async Task Processing_IntroAttemptMessage_Returns_Errors()
+        {
+            await SendMessage<IWebInboundMessage>(new IntroAttemptMessage(_session, _address, _signature));
+            await ReceiveMessage<IOutboundMessage, IntroConfirmationMessage>(message =>
+            {
+                message.Errors.Length.Should().BeGreaterThan(0);
+                message.UserId.Should().Be(default);
+                message.UserName.Should().Be(string.Empty);
+                message.Session.Should().Be(_session);
+            });
+        }
+    }
+}
